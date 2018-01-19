2d8d687df7a129625e40a06ab5f2c9179c54f65c
puffballinthedark <bitsandbytes001@gmail.com>
2018-01-18 21:35:19 -0500

    got auth working

--- primarynerd/__init__.py
+++ primarynerd/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, session
 from flask_pyoidc.flask_pyoidc import OIDCAuthentication
+import os
+
 app = Flask(__name__)
 
 if os.path.exists(os.path.join(os.getcwd(), "config.py")):
@@ -8,16 +10,24 @@ if os.path.exists(os.path.join(os.getcwd(), "config.py")):
 else:
     app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
 
-
 auth = OIDCAuthentication(app,
                           issuer=app.config['OIDC_ISSUER'],
                           client_registration_info=app.config['OIDC_CLIENT_CONFIG'])
+def get_metadata():
+    uuid = str(session["userinfo"].get("sub", ""))
+    uid = str(session["userinfo"].get("preferred_username", ""))
+    metadata = {
+        "uuid": uuid,
+        "uid": uid,
+    }
+    return metadata
+
 
 @app.route("/")
 @auth.oidc_auth
 def hello():
-    return render_template("base.html")
-
+    metadata = get_metadata()
+    return render_template("base.html", metadata=metadata)
 
 @app.route('/logout')
 @auth.oidc_logout
